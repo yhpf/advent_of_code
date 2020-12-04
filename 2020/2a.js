aa151dcf70e48411202a1c43db10e56c33fbb09a
Youn Hee Pernling Frodin <yonipernling@gmail.com>
2020-12-03 20:28:38 -0500

    solved day 2 part 1

--- 2020/2a.js
+++ 2020/2a.js
@@ -1,11 +1,9 @@
 
 // Advent of Code, Day 2 part 1 - How many passwords are correct? 
 
-// Read file
-
+// READ FILE
 const fs = require("fs");
-
-let content = fs.readFileSync('2b-input.txt', 'utf8');
+let content = fs.readFileSync('2a-input.txt', 'utf8');
 
 
 // FIND KEY LETTER //
@@ -13,35 +11,43 @@ let content = fs.readFileSync('2b-input.txt', 'utf8');
 // Split on new line
 let uppdelatRad = content.split("\n");
 
-// Split on white space
-let uppdelatOrd = uppdelatRad[0].split(" ")
+// Add counter
+let counter = 0;
 
-// Remove kolon from letter 
-let keyLetter = uppdelatOrd[1][0];
+// Iterate over every row with slitted values in the text file
+for (let i = 0; i < uppdelatRad.length; i++) {  
 
+	// Split on white space
+	let uppdelatOrd = uppdelatRad[i].split(" ")
 
-// FIND MIN AND MAX NUMBER OF TIMES KEY CAN APPEAR
-
-// Seperate min and max from each other
-let sepMinMax = uppdelatOrd[0].split("-")
-
-// Set min and max
-
-let minimum = sepMinMax[0]
-let maximum = sepMinMax[1]
+	// Remove kolon from letter 
+	let keyLetter = uppdelatOrd[1][0];
 
 
-// EVALUATE IF THE PASSWORD IS CORRECT 
+	// FIND MIN AND MAX NUMBER OF TIMES KEY CAN APPEAR
 
-// How many times can we find key letter in password
-let numAppear = uppdelatOrd[2].split(keyLetter).length - 1
+	// Seperate min and max from each other
+	let sepMinMax = uppdelatOrd[0].split("-")
 
-// Check if the number of apperence of the letter is within the span om min and max
-if (numAppear >= minimum && numAppear <= maximum) {
-	console.log('Password OK')
-} else {
-	console.log('Not valid password')
+	// Set min and max
+	let minimum = sepMinMax[0]
+	let maximum = sepMinMax[1]
+
+
+	// EVALUATE IF THE PASSWORD IS CORRECT 
+
+	// How many times can we find key letter in password
+	let numAppear = uppdelatOrd[2].split(keyLetter).length - 1
+
+	// Check if the number of apperence of the letter is within the span om min and max, and add to counter if password ok
+	if (numAppear >= minimum && numAppear <= maximum) {
+		counter++;
+	}
 }
 
-// need to make a function and loop and also count OK passwords instead of writing it out
+// SHOW COUNTER
+console.log(counter);
+
+
+
 
