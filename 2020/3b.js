332ec14d3f0dc75eddcf5000aed337f2911ea69b
Youn Hee Pernling Frodin <yonipernling@gmail.com>
2020-12-07 15:58:51 -0500

    used a function in the solution

--- 2020/3b.js
+++ 2020/3b.js
@@ -1,6 +1,5 @@
 
 // Advent of Code, Day 3 part 2 - Toboggan Trajectory
-// This was a really ugly solution, but it solved the problem :)
 
 // READ FILE
 const fs = require("fs");
@@ -8,102 +7,30 @@ let content = fs.readFileSync('3a-input.txt', 'utf8');
 
 // Split textfile into lines
 let rows = content.split("\n");
+
+// Define the width of the content row
 let width = rows[0].length;
 
 
-// MOVE IN THE MAP - Right 1, down 1
+// Make a function 
+function moveAround(dx, dy) {
+	// Add counter for trees
+	let treeCounter = 0;
 
-// Add counter for trees
-let treeCounter1 = 0;
+	// Creat a variable for moving sideways on the row
+	let x = 0;
 
-// Creat a variable for moving sideways on the row
-let x = 0;
-
-// Move 1 to the right (with help of x) and 1 down (with help of y)
-
-for (let y = 0; y < rows.length; y++) {
-	if (rows[y][x%width] === '#') {
-		treeCounter1++;
+	// For loop, Move dx to the right and dy down, add to counter if #
+	for (let y = 0; y < rows.length; y+=dy) {
+		if (rows[y][x%width] === '#') {
+			treeCounter++;
+		}
+		x+=dx;
 	}
-	x+=1;
+	return treeCounter;
 }
 
-
-// MOVE IN THE MAP - Right 3, down 1
-
-// Add counter for trees
-let treeCounter2 = 0;
-
-// Set x to 0 again
-x = 0;
-
-// Move 3 to the right (with help of x) and 1 down (with help of y)
-
-for (let y = 0; y < rows.length; y++) {
-	if (rows[y][x%width] === '#') {
-		treeCounter2++;
-	}
-	x+=3;
-}
-
-
-// MOVE IN THE MAP - Right 5, down 1
-
-// Add counter for trees
-let treeCounter3 = 0;
-
-// Set x to 0 again
-x = 0;
-
-// Move 5 to the right (with help of x) and 1 down (with help of y)
-
-for (let y = 0; y < rows.length; y++) {
-	if (rows[y][x%width] === '#') {
-		treeCounter3++;
-	}
-	x+=5;
-}
-
-
-// MOVE IN THE MAP - Right 7, down 1
-
-// Add counter for trees
-let treeCounter4 = 0;
-
-// Set x to 0 again
-x = 0;
-
-// Move 7 to the right (with help of x) and 1 down (with help of y)
-
-for (let y = 0; y < rows.length; y++) {
-	if (rows[y][x%width] === '#') {
-		treeCounter4++;
-	}
-	x+=7;
-}
-
-
-// MOVE IN THE MAP - Right 1, down 2
-
-// Add counter for trees
-let treeCounter5 = 0;
-
-// Set x to 0 again
-x = 0;
-
-// Move 1 to the right (with help of x) and 2 down (with help of y)
-
-for (let y = 0; y < rows.length; y += 2) {
-	if (rows[y][x%width] === '#') {
-		treeCounter5++;
-	}
-	x++;
-}
-
-
-// Multiplied together all the treeCounters
-let answer = treeCounter1 * treeCounter2 * treeCounter3 * treeCounter4 * treeCounter5;
-
-console.log(answer);
+// Call function,  multiplie results  and log 
+console.log(moveAround(1,1) * moveAround(3,1) * moveAround(5,1) * moveAround(7,1) * moveAround(1,2));
 
 
